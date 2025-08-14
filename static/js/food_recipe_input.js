05fd4bee4c2e52e4baa5dcac5c5949a2d34ef561
alskso <eid.alsk91@gmail.com>
2025-08-14 19:18:22 +0900

    Design: recipe_input css 수정

--- static/js/food_recipe_input.js
+++ static/js/food_recipe_input.js
@@ -9,17 +9,30 @@ document.addEventListener('DOMContentLoaded', () => {
     keyboardIcon.style.display = 'none';
     foodBtn.style.display = 'none';
 
+    // input에 글자가 들어올 때
     input.addEventListener('input', () => {
         if (input.value.trim() !== "") {
             keyboardIcon.style.display = 'block';
-            foodBtn.style.display = 'flex'; // 보여주기
+            foodBtn.style.display = 'flex';
             selectBtn.classList.add('hidden');
             gptBox.classList.add('up');
         } else {
             keyboardIcon.style.display = 'none';
-            foodBtn.style.display = 'none'; // 숨기기
+            foodBtn.style.display = 'none';
             selectBtn.classList.remove('hidden');
             gptBox.classList.remove('up');
+        }
+    });
+
+    // document 클릭 이벤트
+    document.addEventListener('click', (e) => {
+        // 클릭된 요소가 input이 아니라면 원래 상태로 되돌림
+        if (!input.contains(e.target)) {
+            keyboardIcon.style.display = 'none';
+            foodBtn.style.display = 'none';
+            selectBtn.classList.remove('hidden');
+            gptBox.classList.remove('up');
+            input.value = ''; // 필요하면 입력값도 초기화
         }
     });
 });
